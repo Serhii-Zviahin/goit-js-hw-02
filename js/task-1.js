488603f0daacf460736c287f4dc89f8575c1c6de
Serhii <kallisto77@gmail.com>
2025-08-17 13:30:50 +0400

    solve task 2

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,10 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
     let totalPrice = quantity * pricePerDroid;
     if (totalPrice > customerCredits) {
-        return `Insufficient funds!`
+        return `Insufficient funds!`;
     } else {
-       return `You ordered ${quantity} droids worth ${totalPrice} credits!`
+        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
     }
-
 }
 
 
